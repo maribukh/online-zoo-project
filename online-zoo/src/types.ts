3f0d788c8574f0c245090b049f8601a721d94c0f
maribukh <mariebukhaidze@gmail.com>
2026-03-09 02:21:28 +0400

    feat: add types for aside bar and petDescription

--- online-zoo/src/types.ts
+++ online-zoo/src/types.ts
@@ -42,3 +42,51 @@ export interface IDonation {
   userName: string;
   userEmail: string;
 }
+
+// for Zoo Page
+
+export interface IPetDetail {
+  id: number;
+  commonName: string;
+  scientificName: string;
+  type: string;
+  size: string;
+  diet: string;
+  habitat: string;
+  range: string;
+  latitude: string;
+  longitude: string;
+  description: string;
+  detailedDescription: string;
+}
+
+export interface CameraItem {
+  id: number;
+  petId: number;
+  text: string;
+}
+
+export interface PetAsset {
+  img: string;
+  icon: string;
+  link: string;
+  mainVideo: string;
+  cams: string[];
+}
+
+interface LeafletMap {
+  setView(latlng: [number, number], zoom: number): LeafletMap;
+}
+interface LeafletMarker {
+  addTo(map: LeafletMap): LeafletMarker;
+  bindPopup(html: string): LeafletMarker;
+  openPopup(): LeafletMarker;
+}
+export interface LeafletStatic {
+  map(id: string): LeafletMap;
+  tileLayer(
+    url: string,
+    options: { attribution: string },
+  ): { addTo(map: LeafletMap): void };
+  marker(latlng: [number, number]): LeafletMarker;
+}
