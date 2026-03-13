44c2867892ffb9ca236c0cd9336897ada0b63e0e
maribukh <mariebukhaidze@gmail.com>
2026-03-13 21:31:59 +0400

    update:

--- online-zoo/vite.config.ts
+++ online-zoo/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+ 
 export default defineConfig({
   root: './',
   base: '/',
@@ -22,3 +22,4 @@ export default defineConfig({
     },
   },
 });
+ 
